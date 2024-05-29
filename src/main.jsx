330a792226c49961dff3a716d2b2dc62e65aefd0
lotus <1504558766@qq.com>
2024-05-29 11:03:19 +0800

    1. init

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import 'virtual:uno.css'
+import './styles/main.scss'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>a</div>,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+)
