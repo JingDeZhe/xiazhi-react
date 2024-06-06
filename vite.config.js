82c4187d99bacd9281a6f193de81e95676072e6a
lotusloli <nymphaeal@qq.com>
2024-06-06 22:34:44 +0800

    1. github page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import cdn from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/xiazhi-react/',
   server: { port: 5182 },
   plugins: [
     react(),
