cdb42ef74f81ad45e15c852fd02e9b9c731b771d
lotusloli <nymphaeal@qq.com>
2024-06-06 20:22:32 +0800

    1. cdn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import cdn from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,25 @@ export default defineConfig({
         },
       ],
       dts: './auto-imports.d.ts',
+    }),
+    cdn({
+      modules: [
+        {
+          name: 'react',
+          var: 'React',
+          path: `https://cdnjs.cloudflare.com/ajax/libs/react/18.3.1/cjs/react.production.min.js`,
+        },
+        {
+          name: 'react-dom',
+          var: 'ReactDOM',
+          path: `https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.3.1/cjs/react-dom.production.min.js`,
+        },
+        {
+          name: 'react-router-dom',
+          var: 'ReactRouterDOM',
+          path: `https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/6.23.1/react-router-dom.production.min.js`,
+        },
+      ],
     }),
   ],
   resolve: {
